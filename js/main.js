85cfece9fec73708e1e532fc529103d2c655f23d
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-12-19 20:05:35 -0500

    added deck id

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+let deckId = "";
+
 fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
   .then((res) => res.json()) // parse response as JSON
   .then((data) => {
     console.log(data);
+    deckId = data.deck_id;
   })
   .catch((err) => {
     console.log(`error ${err}`);
