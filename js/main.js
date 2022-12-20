443b419dd1daa569ff5484b34e299a1570307744
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-12-19 20:03:09 -0500

    basic html, css, and js added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+  .then((res) => res.json()) // parse response as JSON
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
